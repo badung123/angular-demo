591fe6ae403d1a13f12e870f4c755683bc6ab0ac
dungnb <dungnb@gemvietnam.com>
2018-04-20 18:30:15 +0700

    directive

--- src/app/member/department-list/department-list.component.ts
+++ src/app/member/department-list/department-list.component.ts
@@ -9,6 +9,29 @@ export class DepartmentListComponent implements OnInit {
 
   constructor() { }
 
+  departments: any[] = [
+    {
+      'id': 1,
+      'name':'.NET',
+      'manager':'Tran Dinh Giap',
+      'age':35
+      
+    },
+    {
+      'id': 2,
+      'name':'PHP',
+      'manager':'Viet...',
+      'age':30
+    },
+    {
+      'id': 3,
+      'name':'Python',
+      'manager':'Unknown',
+      'age':0
+    }
+  ];
+
+
   ngOnInit() {
   }
 
